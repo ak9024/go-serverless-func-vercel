1ee46648e61978276c64f59fea8e629860154143
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-08-16 10:03:18 +0800

    feat(api): implement image generation endpoint
    Add a new API endpoint to generate random images using the go-chatgpt-sdk library.
    The endpoint is implemented in the Handler function in the api/index.go file.
    
    Added Dependencies:
    - github.com/ak9024/go-chatgpt-sdk v0.0.0-20230810115654-e815c2907c4d
    - github.com/go-resty/resty/v2 v2.7.0
    - golang.org/x/net v0.0.0-20211029224645-99673261e6eb
    
    Changes Made:
    - Imported the necessary packages for the image generation functionality.
    - Created structs Error and Response for handling error and response data.
    - Implemented the Handler function to generate random images using the go-chatgpt-sdk library.
    - Used environment variable OPENAI_API_KEY to set the OpenAI key in the go

--- api/index.go
+++ api/index.go
@@ -1,7 +1,40 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+
+	gochatgptsdk "github.com/ak9024/go-chatgpt-sdk"
+)
+
+type Error struct {
+	StatusCode int    `json:"status_code"`
+	Message    string `json:"message"`
+}
+
+type Response struct {
+	StatusCode int                                                     `json:"status_code"`
+	Data       *gochatgptsdk.ModelImagesResponse[gochatgptsdk.DataURL] `json:"data"`
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`{"message": "ok!"}`))
+	c := gochatgptsdk.NewConfig(gochatgptsdk.Config{
+		OpenAIKey: os.Getenv("OPENAI_API_KEY"),
+	})
+
+	resp, err := c.ImagesGenerations(gochatgptsdk.ModelImages{
+		Prompt: "Generate random images",
+	})
+	if err != nil {
+		json.NewEncoder(w).Encode(Error{
+			StatusCode: http.StatusInternalServerError,
+			Message:    err.Error.Message,
+		})
+	}
+
+	json.NewEncoder(w).Encode(Response{
+		StatusCode: http.StatusOK,
+		Data:       resp,
+	})
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/ak9024/go-serverless-func-vercel
 
 go 1.20
+
+require github.com/ak9024/go-chatgpt-sdk v0.0.0-20230810115654-e815c2907c4d
+
+require (
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
+)
